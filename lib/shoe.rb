bd8dead65d2d69bce9df442fc5b0225e0a81f0ba
Timur Mishiev <timurmishiev@gmail.com>
2020-02-08 02:33:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :size, :brand, :material, :color
+  
+ 
+  def initialize(brand)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
